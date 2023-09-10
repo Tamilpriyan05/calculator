58040b4d4ec441791eb99af048fdcfc8a42bb336
TAMILPRIYAN R <priyanpri555@gmail.com>
2023-09-10 21:03:17 +0530

    third commit
    third

---
+++ calculator/calculator.js
@@ -0,0 +1,30 @@
+ let displayvalue= document.getElementById("inputtext");
+
+ let string="";
+  
+console.log(displayvalue);
+ function buttonclick(number){
+   displayvalue.value+=number;
+}
+
+ function deletebutton(){
+    displayvalue.value=string;
+ }
+
+ function result(){
+   try{
+    displayvalue.value=eval(displayvalue.value);
+   }
+   catch (err){
+    alert("check the correcct method")
+   }
+     
+ }
+
+ function clearbutton(){
+    displayvalue.value=displayvalue.value.slice(0,-1);
+ }
+  
+
+     
+
